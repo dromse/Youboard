bd6fcc1ebbdb64e2e44c5059153d7456571b3530
dromse <dromseproducer@gmail.com>
2022-05-02 02:20:59 +0300

    add middleware for routes, errors

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
-import dotenv from 'dotenv'
+const dotenv = require('dotenv')
 dotenv.config({ path: '.dev.env' })
-import express from 'express'
+
+const express = require('express')
+const cors = require('cors')
+
+const seq = require('./db')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandler')
 
 const PORT = process.env.PORT || 5000
 
-var app = express()
+const app = express()
+
 app.use(express.json())
+app.use(cors())
+app.use('/api', router)
+app.use(errorHandler)
 
 const start = () => {
     try {
+        seq.authenticate()
+        seq.sync()
         app.listen(PORT, () => console.log(`Server start at port: ${PORT}`))
     } catch (e) {
         console.log(e)
