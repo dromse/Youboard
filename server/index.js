45c3f1fb3000611ea9fa33aa18f1ff77216f6ff1
dromse <dromseproducer@gmail.com>
2022-05-02 23:57:54 +0300

    add cookie middleware

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ dotenv.config({ path: '.dev.env' })
 
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 const seq = require('./db')
 const router = require('./routes/index')
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors())
 app.use('/api', router)
 app.use(errorHandler)
