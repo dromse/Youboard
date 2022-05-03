b7ca5f5203827666f509b2619da0c31a88646425
dromse <dromseproducer@gmail.com>
2022-05-03 15:16:40 +0300

    add table and card components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
+import './App.scss'
+import Table from './components/table/table.js'
+
 function App() {
-    return <div>App</div>
+    return (
+        <div className='main'>
+            <h1 className='table-title'>Kanban UI</h1>
+            <Table />
+        </div>
+    )
 }
 
 export default App
---
+++ client/src/components/card/card.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './card.scss'
+
+const Card = (props) => {
+    return <div className='card'>{props}</div>
+}
+
+export default Card
